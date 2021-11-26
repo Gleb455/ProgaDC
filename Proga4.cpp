3f8e0103619869beb130371b8f7b55bfc8f863ba
ElderEv1l <95085670+ElderEv1l@users.noreply.github.com>
2021-11-26 19:23:06 +0300

    Add files via upload

---
+++ Proga4.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int t;
+
+    printf("Введите число: ");
+    scanf("%d", &t);
+
+    printf((t >-100 && t<100) ? "Число в промежутке\n\n" : "Число не в промежутке\n\n");
+
+    printf("Введите число в 8 системе счисления для проверки бита: ");
+    scanf("%o", &t);
+    printf("Девятый бит введённого числа: %d\n", (t >> 8) % 2);
+
+    return 0;
+}
